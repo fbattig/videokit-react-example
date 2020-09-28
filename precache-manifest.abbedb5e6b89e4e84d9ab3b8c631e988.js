1b3f45d2b4c6e9a6a81b0666b753160074b75223
Agustín Díaz <me@adiaz.uy>
2020-09-28 21:44:29 +0200

    Updates

---
+++ precache-manifest.abbedb5e6b89e4e84d9ab3b8c631e988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667d88fce1043ca9ba810f6b3da6276d",
+    "url": "/videokit-react-example/index.html"
+  },
+  {
+    "revision": "55b1dc517902cdb5b678",
+    "url": "/videokit-react-example/static/css/2.b747a192.chunk.css"
+  },
+  {
+    "revision": "3fb86d44ccc09971521e",
+    "url": "/videokit-react-example/static/css/main.6c8e6350.chunk.css"
+  },
+  {
+    "revision": "55b1dc517902cdb5b678",
+    "url": "/videokit-react-example/static/js/2.edc19045.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/videokit-react-example/static/js/2.edc19045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb86d44ccc09971521e",
+    "url": "/videokit-react-example/static/js/main.c0a6945b.chunk.js"
+  },
+  {
+    "revision": "590bded036861460c07a",
+    "url": "/videokit-react-example/static/js/runtime-main.bd2d0568.js"
+  }
+]);
